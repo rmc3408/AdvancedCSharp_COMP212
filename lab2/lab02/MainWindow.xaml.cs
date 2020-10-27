b54f253d968edecd9ce3057f84877bc36f813a42
rmc3408 <rmc3408@protonmail.com>
2020-10-27 00:36:33 -0400

    lab02

---
+++ lab2/lab02/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab02
+{
+
+    
+
+    public class Plant
+    {
+       
+        //public string Fruit { get; set; }
+        //public string Vaca { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        
+
+        public Plant(string n, double pr)
+        {
+            Name = n;
+            Price = pr;
+            
+
+
+        }
+        public override string ToString()
+        {
+            return $"{Name} - ${Price}";
+        }
+
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Plant> packFruits = new ObservableCollection<Plant>();
+        ObservableCollection<Plant> packFlowers = new ObservableCollection<Plant>();
+        ObservableCollection<Plant> selectedPlant = new ObservableCollection<Plant>();
+
+        double totalResult;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            packFruits.Add(new Plant("Plum", 1.30));
+            packFruits.Add(new Plant("Kiwi", 3.85));
+            this.fruitBox.ItemsSource = packFruits;
+
+            packFlowers.Add(new Plant("Lilly", 9.00));
+            packFlowers.Add(new Plant("Rose", 5.50));
+            this.flowerBox.ItemsSource = packFlowers;
+
+            //ListCreation();
+
+            this.dataGrid1.ItemsSource = selectedPlant;
+
+
+            
+
+        }
+
+        private void pickFlower(object sender, SelectionChangedEventArgs e)
+        {
+
+            //flowerBox.IsEditable = true;
+            //flowerBox.IsReadOnly = true;
+            var theFlower = (sender as ComboBox).SelectedItem;
+            if (theFlower == null)
+            {
+                return;
+            }
+            Plant s = (Plant)theFlower;
+            selectedPlant.Add(new Plant(s.Name, s.Price));
+            totalResult += s.Price;
+            num.Text = totalResult.ToString("C2");
+            
+        }
+
+        private void pickFruit(object sender, SelectionChangedEventArgs e)
+        {
+            var theFruit = (sender as ComboBox).SelectedItem;
+            if(theFruit == null)
+            {
+                return;
+            }
+            Plant s = (Plant)theFruit;
+            selectedPlant.Add(new Plant(s.Name, s.Price));
+            totalResult += s.Price;
+            num.Text = totalResult.ToString("C2");
+
+        }
+
+        private void call_Delete(object sender, RoutedEventArgs e)
+        {
+                
+            Plant vr = (Plant)dataGrid1.SelectedItem;
+            if(vr == null) return;
+            totalResult -= vr.Price;
+            selectedPlant.Remove(vr);
+            num.Text = totalResult.ToString("C2");
+
+
+        }
+
+        private void call_Clear(object sender, RoutedEventArgs e)
+        {
+            totalResult = 0;
+            num.Text = totalResult.ToString("C2");
+            flowerBox.Text = "Pick a Flower";
+            fruitBox.Text = "Pick a Fruit";
+            selectedPlant.Clear();
+        }
+
+
+    }
+}
---
+++ lab2/mamadou/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab2_Comp212_Fall20
+{
+    public class DataContent
+    {
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        private double priceInDouble;
+
+
+        public DataContent(string name, double price)
+        {
+            Name = name;
+            this.priceInDouble = price;
+            Price = string.Format("${0:0.00}", price);
+            //Price = price;
+
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ObservableCollection<DataContent> dataContents = new ObservableCollection<DataContent>();
+
+        double totalPrice = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateData();
+            
+        }
+
+        public void PopulateData()
+        {
+            this.LabData1.ItemsSource = dataContents;
+        }
+
+
+
+        private void lilly_Selected(object sender, RoutedEventArgs e)
+        {
+            dataContents.Add(new DataContent("Lilly", 40.00));
+            totalPrice += 40.00;
+            textBlock2.Text = totalPrice.ToString("c");
+            
+        }
+
+        private void rose_Selected(object sender, RoutedEventArgs e)
+        {
+            dataContents.Add(new DataContent("Rose", 30.00));
+            totalPrice += 30.00;
+            textBlock2.Text = totalPrice.ToString("c");
+        }
+
+        private void plum_Selected(object sender, RoutedEventArgs e)
+        {
+            dataContents.Add(new DataContent("Plum", 15.00));
+            totalPrice += 15.00;
+            textBlock2.Text = totalPrice.ToString("c");
+        }
+
+        private void kiwi_Selected(object sender, RoutedEventArgs e)
+        {
+            dataContents.Add(new DataContent("Kiwi", 30.00));
+            totalPrice += 30.00;
+            textBlock2.Text = totalPrice.ToString("c");
+        }
+
+        private void deleteRowButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataContent d = (DataContent)LabData1.SelectedItem;
+            dataContents.Remove(d);
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            dataContents.Clear();
+            flowerComboBox.SelectedIndex = 0;
+            fruitComboBox.SelectedIndex = 0;
+            totalPrice = 0;
+            textBlock2.Text = "0.00";
+        }
+    }
+}
